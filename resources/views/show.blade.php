c2d150b9e606b23690e7a7493499538c018a638f
Ongoo <erdenedawaa@gmail.com>
2020-05-15 10:20:08 +0800

    Second draft

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 
+{{-- test git commit --}}
 @section('content')
     <div class="movie-info border-b border-gray-800">
       <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row ">
